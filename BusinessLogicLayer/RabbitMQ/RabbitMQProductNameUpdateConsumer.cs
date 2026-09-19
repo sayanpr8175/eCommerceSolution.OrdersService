398aff9618a3db5e576b865d2c80d0cf05594714
Sayan Pramanik <41745417+sayanpr8175@users.noreply.github.com>
2026-09-18 23:48:06 -0400

    Updated orders-microservice gateway links to reconcile with azure manifest files.

--- BusinessLogicLayer/RabbitMQ/RabbitMQProductNameUpdateConsumer.cs
+++ BusinessLogicLayer/RabbitMQ/RabbitMQProductNameUpdateConsumer.cs
@@ -27,6 +27,14 @@ public class RabbitMQProductNameUpdateConsumer : IDisposable, IRabbitMQProductNa
         _configuration = configuration;
         _logger = logger;
 
+        Console.WriteLine($"RabbitMQ_HostName: {_configuration["RabbitMQ_HostName"]}");
+
+        Console.WriteLine($"RabbitMQ_UserName: {_configuration["RabbitMQ_UserName"]}");
+
+        Console.WriteLine($"RabbitMQ_Password: {_configuration["RabbitMQ_Password"]}");
+
+        Console.WriteLine($"RabbitMQ_Port: {_configuration["RabbitMQ_Port"]}");
+
         string hostName = _configuration["RabbitMQ_HostName"];
         string userName = _configuration["RabbitMQ_UserName"];
         string password = _configuration["RabbitMQ_Password"];
